4070911597d9cf163992f9f29564489b48e9a459
tuan5 <tuannqph17896@fpt.edu.vn>
2022-06-01 22:16:40 +0700

    MOB104

--- pulic/js/login.js
+++ pulic/js/login.js
@@ -18,7 +18,7 @@ document.getElementById("btn-login").onclick = function () {
             })
             firebase.auth().onAuthStateChanged(function (user) {
                 if (user) {
-                    window.location.href = "home.html";
+                    window.location.href = "home";
                     window.alert("Login successfully")
                 }
             });
