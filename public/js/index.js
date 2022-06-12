24059bd0b35eca0fee25537a1a171736edb2217f
tuan5 <tuannqph17896@fpt.edu.vn>
2022-06-12 20:36:12 +0700

    MOB104

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,8 @@
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: "AIzaSyDPJUs2or3u-TIzsksHiNCFzaHLlW8iQW0",
     authDomain: "fir-4d080.firebaseapp.com",
